06278a63696c442fc200ff9119eb7e0a082501c8
userowu <osw9657@nyu.edu>
2022-11-01 14:55:26 -0400

    Saving my work 2

--- 05-randomWords.js
+++
@@ -1,9 +0,0 @@
-
-array = ['mother', 'bunny', 'prom', 'chocolate']
-var arrayLen = array.length;
-
-function randSentence() {
-  return 'my ' + array[Math.random(0, arrayLen)] + ' ' + 'stopped working because his ' + array[Math.random(0, arrayLen)] + ' stepped on the ' + array[Math.random(0, arrayLen)];
-}
-
-console.log(randSentence());
